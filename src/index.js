48736dafd3dcd4e7a08521c4d46ed46aa997f058
Vlad Trostinsky <gribennikov.vlad@gmail.com>
2018-05-15 16:25:09 +0300

    Add buttons to change books type

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import HelloClassState, {Hello, HelloClassProps} from "./hello";
 import registerServiceWorker from './registerServiceWorker';
 import "./index.css";
 
-
+const BookItem = ({name, author, cover}) => (
+    <div className="book">
+        <div className="book__name">
+            {name}
+        </div>
+        <div className="book__author">
+            {author}
+        </div>
+        <img src={cover}
+             alt="Book cover"
+             className="book__image"/>
+    </div>
+)
 class Books extends Component{
     // Автоматически вызывается super() === new Component();
     state = {
-        books: [{
+        javascriptBooks: [{
             name: "JavaScript: The Definitive Guide: The Definitive Guide",
             author: "D. Flanagan",
             cover: "/1.jpg"
@@ -20,32 +32,40 @@ class Books extends Component{
             name: "Modular Programming with JavaScript",
             author: "Sasan Seydnejad",
             cover: "/3.jpg"
-        }]
+        }],
+        pythonBooks: [{
+            name: "Programming Python",
+            author: "Mark Lutz",
+            cover: "/4.jpg"
+        }, {
+            name: "Learning Python: Powerful Object-Oriented Programming",
+            author: "Mark Lutz",
+            cover: "/5.jpg"
+        }],
+        active: 'javascript'
     }
 
     // React Children can be:
     // Primitve(String, Number, Boolean), Components, Elements
     // and Array of (Primitive, Components, Elements)
+    changeBook(active){
+        this.setState({
+            active
+        })
+    }
 
     render(){
-        const {books} = this.state;
+        const books = this.state[this.state.active + "Books"];
         return (
             <div className="books__wrapper">
-                {books.map((book, index) => {
-                    return (
-                        <div className="book" key={index}>
-                            <div className="book__name">
-                                {book.name}
-                            </div>
-                            <div className="book__author">
-                                {book.author}
-                            </div>
-                            <img src={book.cover}
-                                 alt="Book cover"
-                                 className="book__image"/>
-                        </div>
-                    )
-                })}
+                <h2>My favorite books</h2>
+                <button onClick={this.changeBook.bind(this, "python")}>
+                    Python Books
+                </button>
+                <button onClick={this.changeBook.bind(this, "javascript")}>
+                    Javascript Books
+                </button>
+                {books.map((book, index) => <BookItem {...book} /> )}
             </div>
         )
     }
