2b86d34701235e070c53e504a79a39415c2ae8a3
user <gribennikov.vlad@gmail.com>
2018-05-16 13:29:31 +0300

    Working with state and inputs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import HelloClassState, {Hello, HelloClassProps} from "./hello";
+import HelloClassState, {Hello, HelloClassProps} from "./hello"
 import registerServiceWorker from './registerServiceWorker';
 import "./index.css";
 
@@ -20,30 +20,39 @@ const BookItem = ({name, author, cover}) => (
 class Books extends Component{
     // Автоматически вызывается super() === new Component();
     state = {
-        javascriptBooks: [{
+        books: [{
             name: "JavaScript: The Definitive Guide: The Definitive Guide",
             author: "D. Flanagan",
+            type: "javascript",
             cover: "/1.jpg"
         }, {
             name: "JavaScript: The Good Parts: The Good Parts",
             author: "Douglas Crockford",
+            type: "javascript",
             cover: "/2.jpg"
         }, {
             name: "Modular Programming with JavaScript",
             author: "Sasan Seydnejad",
+            type: "javascript",
             cover: "/3.jpg"
-        }],
-        pythonBooks: [{
+        }, {
             name: "Programming Python",
             author: "Mark Lutz",
+            type: "python",
             cover: "/4.jpg"
         }, {
             name: "Learning Python: Powerful Object-Oriented Programming",
             author: "Mark Lutz",
+            type: "python",
             cover: "/5.jpg"
         }],
-        active: 'javascript'
+        active: 'javascript',
+        name: "",
+        author: "",
+        cover: "/missing.jpg",
+        value: 0
     }
+
 
     // React Children can be:
     // Primitve(String, Number, Boolean), Components, Elements
@@ -54,8 +63,48 @@ class Books extends Component{
         })
     }
 
+    changeInput(field, event) {
+        const value = event.target.value;
+        this.setState({
+            [field]: value
+        });
+    }
+
+
+    // Perfomance improve solution
+    changeName(event) {
+        this.changeInput.call(this, "name", event);
+    }
+
+
+
+    addBook = () => {
+        // Если важно предыдущее состояние, то используете функцию в первом аргументе
+        // Пример:
+        // this.setState((prevState) => {
+        //     return {
+        //         // состояние основанное на предыдущем
+        //     }
+        // })
+
+        // Если неважно, используете объект с новым состоянием в setState();
+        // Пример:
+        // this.setState({
+        //     // новое состояние
+        // })
+
+        this.setState((prevState) => ({
+            javascriptBooks: [...prevState.javascriptBooks, {
+                name: prevState.name,
+                cover: prevState.cover,
+                author: prevState.author
+            }]
+        }))
+    }
+
+
     render(){
-        const books = this.state[this.state.active + "Books"];
+        const books = [...this.state.books].filter((book) => book.type === this.state.active);
         return (
             <div className="books__wrapper">
                 <h2>My favorite books</h2>
@@ -65,6 +114,32 @@ class Books extends Component{
                 <button onClick={this.changeBook.bind(this, "javascript")}>
                     Javascript Books
                 </button>
+                <div className="book__add">
+                    <div>
+                        Name:
+                        <input type="text" className="book__name"
+                               value={this.state.name}
+                               onChange={this.changeInput.bind(this, "name")}
+                        />
+                    </div>
+                    <div>
+                        Author:
+                        <input type="text"
+                               className="book__author"
+                               value={this.state.author}
+                               onChange={this.changeInput.bind(this, "author")}
+                        />
+                    </div>
+                    <div>
+                        Cover:
+                        <input type="text"
+                               className="book__cover"
+                               value={this.state.cover}
+                               onChange={this.changeInput.bind(this, "cover")}
+                        />
+                    </div>
+                    <button onClick={this.addBook}>Add Book!</button>
+                </div>
                 {books.map((book, index) => <BookItem {...book} /> )}
             </div>
         )
